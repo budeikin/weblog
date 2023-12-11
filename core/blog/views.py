7e9187979ebe54ce36937855d6127ea410005440
budeikin <budeikin52@gmail.com>
2023-12-11 11:41:42 +0330

    adding post list and post detail

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 # Create your views here.
+
+def post_list(request):
+    all_posts = Post.published.all()
+    context = {
+        'posts': all_posts
+    }
+    return render(request, 'blog/post/post_list.html', context)
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    context = {'post': post}
+    return render(request, 'blog/post/post_detail.html', context)

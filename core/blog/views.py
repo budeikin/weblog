ff37acef91f79440ae020c6443549d3dc1b0cce7
budeikin <budeikin52@gmail.com>
2023-12-13 16:19:46 +0330

    adding class-based views for blog

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,26 +1,56 @@
-from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
+from .forms import EmailPostForm
 
 
 # Create your views here.
-
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    return render(request, 'blog/post/post_list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    paginate_by = 3
+    context_object_name = 'posts'
+    template_name = 'blog/post/post_list.html'
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, publish__year=year, publish__month=month, publish__day=day, slug=post,
-                             status=Post.Status.PUBLISHED)
-    context = {'post': post}
-    return render(request, 'blog/post/post_detail.html', context)
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     return render(request, 'blog/post/post_list.html', {'posts': posts})
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post/post_detail.html'
+
+    def get_object(self):
+        post = get_object_or_404(Post, publish__year=self.kwargs['year'], publish__month=self.kwargs['month'],
+                                 publish__day=self.kwargs['day'], slug=self.kwargs['post'],
+                                 status=Post.Status.PUBLISHED)
+
+        return post
+
+
+# def post_detail(request, year, month, day, post):
+#     post = get_object_or_404(Post, publish__year=year, publish__month=month, publish__day=day, slug=post,
+#                              status=Post.Status.PUBLISHED)
+#     context = {'post': post}
+#     return render(request, 'blog/post/post_detail.html', context)
+#
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
--- core/core/settings.py
+++ core/core/settings.py
@@ -117,3 +117,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email setting
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'budeikin52@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwwaflyecbtwpwii'
+EMAIL_PORT = 587

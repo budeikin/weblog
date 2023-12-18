1691d47dd62bea765cddcd0d3970ad2115850dad
budeikin <budeikin52@gmail.com>
2023-12-18 10:58:46 +0330

    adding sitemap

--- core/core/settings.py
+++ core/core/settings.py
@@ -28,6 +28,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SITE_ID = 1
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # internal modules
     'blog.apps.BlogConfig',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     # external modules
     'taggit'
 ]

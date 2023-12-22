30ac690b9374b96e2225b8b827aee338492ea1bd
budeikin <budeikin52@gmail.com>
2023-12-22 10:40:43 +0330

    ready for push

--- core/blog/views.py
+++ core/blog/views.py
@@ -7,7 +7,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
 from taggit.models import Tag
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 # Create your views here.
@@ -132,7 +132,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.objects.annotate(search=SearchVector('title', 'body')).filter(search=query)
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.objects.annotate(search=search_vector,
+                                            rank=SearchRank(search_vector, search_query)).filter(
+                search=search_query).order_by('-rank')
 
     return render(request,
                   'blog/post/search.html',
